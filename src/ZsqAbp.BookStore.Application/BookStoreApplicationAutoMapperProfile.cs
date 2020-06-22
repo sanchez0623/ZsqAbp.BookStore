292fb7542b0afacce15e0fc88106dac7f9253b11
sanchez0623 <18576645357@163.com>
2020-06-22 14:32:07 +0800

    Add Book Dto

--- src/ZsqAbp.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
+++ src/ZsqAbp.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -9,6 +9,9 @@ namespace ZsqAbp.BookStore
             /* You can configure your AutoMapper mapping configuration here.
              * Alternatively, you can split your mapping configurations
              * into multiple profile classes for a better organization. */
+
+            CreateMap<Book, BookDto>();
+            CreateMap<CreateUpdateBookDto, Book>();
         }
     }
 }
